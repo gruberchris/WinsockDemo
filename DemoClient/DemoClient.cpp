104d07bbd9aad8a745a0803d5e73d0b574f0528c
Chris Gruber <chris@chrisgruber.com>
2017-09-14 20:56:10 -0500

    Replaced inet_addr. Use c++ cast over c-style. Prefer use auto over explicit type declaration

--- DemoClient/DemoClient.cpp
+++ DemoClient/DemoClient.cpp
@@ -1,23 +1,23 @@
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
-
 #include <iostream>
 #include <winsock2.h>
+#include <ws2tcpip.h>
 
 using namespace std;
 
 int main()
 {
-	WSADATA WSAData;
+	WSADATA wsa_data;
 	SOCKADDR_IN addr;
 
-	WSAStartup(MAKEWORD(2, 0), &WSAData);
-	SOCKET server = socket(AF_INET, SOCK_STREAM, 0);
+	WSAStartup(MAKEWORD(2, 0), &wsa_data);
+	const auto server = socket(AF_INET, SOCK_STREAM, 0);
 
-	addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // replace the ip with your future server ip address. If server AND client are running on the same computer, you can use the local ip 127.0.0.1
+	InetPton(AF_INET, "127.0.0.1", &addr.sin_addr.s_addr);
+
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(5555);
 
-	connect(server, (SOCKADDR *)&addr, sizeof(addr));
+	connect(server, reinterpret_cast<SOCKADDR *>(&addr), sizeof(addr));
 	cout << "Connected to server!" << endl;
 
 	char buffer[1024] = { 'h', 'e', 'l', 'l', 'o', '.' };
