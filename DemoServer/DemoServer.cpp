7db857769d93be940bea70bfda26195bbfc25ac5
Chris Gruber <chris@chrisgruber.com>
2017-09-14 23:13:30 -0500

    server example is multi threaded on client connect

--- DemoServer/DemoServer.cpp
+++ DemoServer/DemoServer.cpp
@@ -1,38 +1,57 @@
+#include <future>
 #include <iostream>
-#include <winsock2.h>
+
+#include "DemoServer.h"
 
 using namespace std;
 
 int main()
 {
 	WSADATA wsa_data;
-	SOCKET client;
 	SOCKADDR_IN server_addr, client_addr;
 
-	WSAStartup(MAKEWORD(2, 0), &wsa_data);
+	WSAStartup(MAKEWORD(2, 2), &wsa_data);
 	const auto server = socket(AF_INET, SOCK_STREAM, 0);
 
 	server_addr.sin_addr.s_addr = INADDR_ANY;
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(5555);
 
-	bind(server, reinterpret_cast<SOCKADDR *>(&server_addr), sizeof(server_addr));
+	::bind(server, reinterpret_cast<SOCKADDR *>(&server_addr), sizeof(server_addr));
 	listen(server, 0);
 
 	cout << "Listening for incoming connections..." << endl;
 
-	char buffer[1024];
 	int client_addr_size = sizeof(client_addr);
 
-	if ((client = accept(server, reinterpret_cast<SOCKADDR *>(&client_addr), &client_addr_size)) != INVALID_SOCKET)
+	for (;;)
 	{
-		cout << "Client connected!" << endl;
-		recv(client, buffer, sizeof(buffer), 0);
-		
-		cout << "Client says: " << buffer << endl;
-		memset(buffer, 0, sizeof(buffer));
+		SOCKET client;
 
-		closesocket(client);
-		cout << "Client disconnected." << endl;
+		if ((client = accept(server, reinterpret_cast<SOCKADDR *>(&client_addr), &client_addr_size)) != INVALID_SOCKET)
+		{
+			auto fut = async(launch::async, on_client_connect, client);
+		}
+
+		const auto last_error = WSAGetLastError();
+		
+		if(last_error > 0)
+		{
+			cout << "Error: " << last_error << endl;
+		}
 	}
+}
+
+void on_client_connect(SOCKET client)
+{
+	char buffer[1024];
+
+	cout << "Client connected!" << endl;
+	recv(client, buffer, sizeof(buffer), 0);
+
+	cout << "Client says: " << buffer << endl;
+	memset(buffer, 0, sizeof(buffer));
+
+	closesocket(client);
+	cout << "Client disconnected." << endl;
 }
---
+++ DemoServer/DemoServer.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <winsock2.h>
+
+void on_client_connect(SOCKET client);
